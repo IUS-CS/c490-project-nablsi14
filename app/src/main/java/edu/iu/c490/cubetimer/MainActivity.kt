45678b59865e217ef6ddec37e5f9a9773f3980be
nablsi14 <nathaniel.b.sigafoos@gmail.com>
2019-11-11 16:58:25 -0500

    Setup skeletion layout

--- app/src/main/java/edu/iu/c490/cubetimer/MainActivity.kt
+++ app/src/main/java/edu/iu/c490/cubetimer/MainActivity.kt
@@ -2,11 +2,54 @@ package edu.iu.c490.cubetimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+import androidx.constraintlayout.widget.ConstraintLayout
 
-class MainActivity : AppCompatActivity() {
+
+private const val TAG = "MAIN"
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    private val fragmentContainer by lazy { findViewById<ConstraintLayout>(R.id.fragment_container) }
+    private val puzzleSelector by lazy { findViewById<Spinner>(R.id.puzzle_selector) }
+    private val timerBtn by lazy { findViewById<Button>(R.id.timer_btn) }
+    private val timeListBtn by lazy { findViewById<Button>(R.id.time_list_btn) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Setup the puzzle selector Spinner
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.puzzle_names,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            puzzleSelector.adapter = adapter
+            puzzleSelector.onItemSelectedListener = this
+        }
+
+        val timerFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if (timerFragment == null) {
+            val fragment = TimerFragment()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
+
+
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
+        Log.d(TAG,"Item Selected: ${parent?.getItemAtPosition(pos)}")
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        Log.e(TAG, "ERROR: No item selected")
     }
 }
